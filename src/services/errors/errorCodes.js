130a03dc5f739b755acbc99ad02892256104f8e0
Federico Gomez <fedehgz@hotmail.com>
2024-06-06 12:44:41 -0300

    Agregué algunos errores comunes al diccionario de errores de ErrorCodes.

--- src/services/errors/errorCodes.js
+++ src/services/errors/errorCodes.js
@@ -2,6 +2,9 @@ const ErrorCodes = {
     ROUTING_ERROR: "1",
     INVALID_TYPES_ERROR: "2",
     DATABASE_ERROR: "3",
+    AUTHENTICATION_ERROR: "4",
+    AUTHORIZATION_ERROR: "5",
+    VALIDATION_ERROR: "6",
 }
 
 module.exports = { ErrorCodes }
